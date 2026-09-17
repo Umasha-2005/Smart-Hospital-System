85da2af9f61ab2eb274ed719e1a5df22fdf67457
Umasha <umashasammani2005@gmail.com>
2026-09-17 22:27:11 +0530

    feat: set up global parallel arrays and constants with max 80 limits

--- main.c
+++ main.c
@@ -2,6 +2,54 @@
 #include <stdio.h>
 #include <string.h>
 
+#define maxPatients 80
+#define numberOfSpecialties 4
+#define numberOfWards 4
+#define maxBeds 20
+
+// specialties data
+
+int specialtyId[4] = {1,2,3,4};
+char specialtyName[4][30] = {"General Practice", "Paediatrics",
+                             "Cardiology", "Neurology"};
+float consultationFee[4] = {1500.00, 2500.00,
+                            4500.00, 5000.00};
+int consultationTime[4] = {15, 20, 30, 30};
+int dailyPatientCapacity[4] = {30, 20, 12, 10};
+
+// wards data
+
+int wardId[4] = {1, 2, 3, 4};
+char wardName[4][30] = {"General Ward", "Paediatric Ward",
+                        "Surgical Ward", "ICU"};
+float wardDailyRate[4] = {3000.00, 6000.00, 12000.00,
+                          25000.00};
+int wardCapacity[4] = {20, 10, 10, 5};
+int bedOccupancy[4][20];
+
+int patientId[maxPatients];
+char patientName[maxPatients][50];
+int patientAge[maxPatients];
+int patientUrgency[maxPatients];
+int patientSpecialty[maxPatients];
+int patientAdmitted[maxPatients];
+int patientWard[maxPatients];
+int patientDays[maxPatients];
+int patientBed[maxPatients];
+int registrationOrder[maxPatients];
+float baseFee[maxPatients];
+float emergencySurcharge[maxPatients];
+float wardCost[maxPatients];
+float grossTotal[maxPatients];
+float discount[maxPatients];
+float finalPayable[maxPatients];
+float waitingTime[maxPatients];
+
+int specialtyQueue[4] = {0, 0, 0, 0};
+int patientCount = 0;
+int nextPatientId = 1001;
+
+
 int main()
 {
     printf("Smart Hospital System Loaded.\n");
